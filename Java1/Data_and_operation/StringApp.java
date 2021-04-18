ff2bbf3ca9b21feecd6c6666321b6de81972d0f1
movegun1027 <94dglee@naver.com>
2021-04-18 09:55:35 +0900

    String VS Character
    String과 Character 차이
    줄바꿈
    escape - String에서 특수문자 사용하기

---
+++ Java1/Data_and_operation/StringApp.java
@@ -0,0 +1,23 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		// Character VS String
+		System.out.println("Hello World"); // String
+//		System.out.println('Hello World'); // error
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		// new line
+		System.out.println("Hello "
+				+ "World"); // ¡ŸπŸ≤ﬁ¿Ã æ»µ 
+		System.out.println("Hello \nWorld"); // ¡ŸπŸ≤ﬁ¿Ã µ 
+		
+		// escape
+//		System.out.println("Hello "World"); // error
+		System.out.println("Hello \"World\""); // Hello "World"
+
+	}
+
+}
