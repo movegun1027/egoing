cf354de6962bacc4647cd1a3ff076a2a2e7f04ef
movegun1027 <94dglee@naver.com>
2021-04-18 10:29:14 +0900

    Letter
    - String 변수를 쓰는 법
    - 바람직한 코드 작성법(변수)

---
+++ Java1/Data_and_operation/Letter.java
@@ -0,0 +1,16 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, egoing ... egoing ... egoing ... bye"); // want to change egoing
+		
+		String name = "egoing";
+		System.out.println("Hello, "+name+" ... "+name+" ... "+name+" ... bye"); // same result
+		
+		System.out.println(10.0); // if someone see this code, do not understand 10.0
+		double VAT = 10.0;
+		System.out.println(VAT); // Anyone can tell what this code means
+	}
+
+}
