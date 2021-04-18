f185c6ce39eaeab125e632a7932a943d9bf103ef
movegun1027 <94dglee@naver.com>
2021-04-18 10:04:36 +0900

    String Operation
    - 문자열의 길이 구하기 "".length()
    - 문자열 중에 한 문자열을 원하는 문자열로 바꾸기 "".replace("바꿀 문자열", "바꾼 문자열")

---
+++ Java1/Data_and_operation/StringOperation.java
@@ -0,0 +1,12 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, leezche ... bye. ".replace("leezche", "egoing")); // Hello, egoing ... bye. 
+		System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "egoing")); // Hello, egoing ... bye.
+
+	}
+
+}
