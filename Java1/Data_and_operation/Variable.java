f28be401a3cf802e26979f2b1c00c39df2672f2f
movegun1027 <94dglee@naver.com>
2021-04-18 10:14:33 +0900

    Variable
    Java의 변수를 지정하는 datatype
    정수 - int
    실수 - double
    문자열 - String

---
+++ Java1/Data_and_operation/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer /// -2, -1, 0, 1 ...
+		System.out.println(a); // 1
+		
+		double b = 1.1; // real number -> double ... -2.0, -1.4, -1, 0, 1.1 ...
+		System.out.println(b); // 1.1
+		
+		String c = "Hello World";
+		System.out.println(c); // Hello World
+
+	}
+
+}
