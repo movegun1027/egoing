5d623ab158bed14c73ceb8855a2e94eae8a57eee
movegun1027 <94dglee@naver.com>
2021-04-21 10:47:11 +0900

    program
    program 동작에 이해

---
+++ Java1/Programming/Program.java
@@ -0,0 +1,16 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// imagine this code has many lines
+		// and we do not know when this program ends
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		
+		// computer automatically sequentially executes code after we wrote code코드를 짜면 기계가 자동으로 순차적으로 코드를 실행
+
+	}
+
+}
